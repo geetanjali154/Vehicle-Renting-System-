8da80097fc040a1010096da2f4c998d027774867
MANBEERGIL <manbeergill545@gmail.com>
2020-02-10 20:35:56 -0500

    Initial Commit

---
+++ Vichle  Renting System/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Vichle  Renting System
+//
+//  Created by MANBEER KAUR on 2020-02-10.
+//  Copyright © 2020 MANBEER KAUR. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
